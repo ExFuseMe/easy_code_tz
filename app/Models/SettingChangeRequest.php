710ee7296a54e22b323d6febb072d95735947dd0
ExFuseMe <dn007228@gmail.com>
2025-02-22 17:23:59 +0300

    fill models

--- app/Models/SettingChangeRequest.php
+++ app/Models/SettingChangeRequest.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SettingChangeRequest extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'setting_id',
+        'method',
+        'old_value',
+        'new_value',
+        'verification_code',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function setting(): BelongsTo
+    {
+        return $this->belongsTo(Setting::class);
+    }
 }
